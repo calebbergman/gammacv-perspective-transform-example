8105e9f0c7bf722354ecf9b16338eb115453bc6a
Caleb Bergman <cbergman@trans-system.com>
2019-08-01 13:59:04 -0700

    Make life easier and use a smaller image. Removed FileSystem modules and methods

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="container">
+    <textarea rows="10" cols="100" v-model="data"></textarea>
+
     <h3>TRANSFORMED</h3>
     <div ref="result">
       <!-- Transformed canvas injected here -->
@@ -8,14 +10,12 @@
     <h3>ORIGINAL</h3>
     <canvas ref="canvas"></canvas>
 
-    <img src="/doc.png" ref="image" v-show="false" />
+    <img src="/doc-small.jpg" ref="image" v-show="false" />
   </div>
 </template>
 
 <script>
 import * as gm from "gammacv";
-import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
-const { Filesystem } = Plugins;
 
 export default {
   name: "HelloWorld",
@@ -23,12 +23,19 @@ export default {
   data: () => ({
     tensorTransform: null,
     vertices: {
-      tl: { x: 234, y: 213 },
-      tr: { x: 1846, y: 211 },
-      br: { x: 1835, y: 2761 },
-      bl: { x: 225, y: 2755 }
-    }
+      tr: { x: 301, y: 42 },
+      tl: { x: 102, y: 5 },
+      bl: { x: 55, y: 279 },
+      br: { x: 254, y: 315 }
+    },
+    output: {}
   }),
+
+  computed: {
+    data() {
+      return (this.output.data || []);
+    }
+  },
 
   methods: {
     doTransform() {
@@ -38,7 +45,7 @@ export default {
       );
       this.$refs.result.appendChild(documentCanvas);
     },
-    async transform(input, { tl, tr, br, bl }) {
+    transform(input, { tl, tr, br, bl }) {
       // The Rect TL, TR, BR, BL
       const rect = new gm.Rect(tl.x, tl.y, tr.x, tr.y, br.x, br.y, bl.x, bl.y);
 
@@ -64,11 +71,7 @@ export default {
       sess.init(operation);
       sess.runOp(operation, 0, output);
 
-      const dir = await this.readdir("gammacv")
-      if (!dir)
-        await this.mkdir("gammacv")
-
-      this.fileWrite("gammacv", output.data.slice(0, 100))
+      this.output = output;
 
       // show output
       const canvas = document.createElement("canvas");
@@ -77,45 +80,6 @@ export default {
       gm.canvasFromTensor(canvas, output);
 
       return canvas;
-    },
-    fileWrite(dir, data) {
-      try {
-        Filesystem.writeFile({
-          path: `${dir}/output.txt`,
-          data: data.toString(),
-          directory: FilesystemDirectory.Documents,
-          encoding: FilesystemEncoding.UTF8
-        })
-      } catch(e) {
-        console.error('Unable to write file', e);
-      }
-    },
-    async fileDelete(path) {
-      await Filesystem.deleteFile({
-        path,
-        directory: FilesystemDirectory.Documents
-      });
-    },
-    async mkdir(dir) {
-      try {
-        let ret = await Filesystem.mkdir({
-          path: dir,
-          directory: FilesystemDirectory.Documents,
-          createIntermediateDirectories: false // like mkdir -p
-        });
-      } catch(e) {
-        console.error('Unable to make directory', e);
-      }
-    },
-    async readdir(dir) {
-      try {
-        let ret = await Filesystem.readdir({
-          path: dir,
-          directory: FilesystemDirectory.Documents
-        });
-      } catch(e) {
-        console.error('Unable to read dir', e);
-      }
     }
   },
 
